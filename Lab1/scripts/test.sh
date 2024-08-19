08a0d1764a6153a6d529c8d8292c66a2e9d9eca3
PRO-GUNE <chathuragunasekera@gmail.com>
2024-08-19 15:50:17 +0530

    Modularized code and scripts. To do visualization script

--- Lab1/scripts/test.sh
+++ Lab1/scripts/test.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# Clean all object files and executables
-make clean
-
-# Compile the C files using the make command
-make all
-
 # List of test files
-test_files=("serial.o" "mutex.o" "rwlock.o")
+obj_files=("../bin/serial.o" "../bin/mutex.o" "../bin/rwlock.o")
 
 # List of command line parameters
 parameters=("1000 10000 0.99 0.005 0.005 1" "1000 10000 0.9 0.05 0.05 1" "1000 10000 0.5 0.25 0.25 1")
-logfiles=("serial.log" "mutex.log" "rwlock.log")
+logfiles=("../logs/serial.log" "../logs/mutex.log" "../logs/rwlock.log")
 numThreads=("1" "2" "4" "8")
 
 # Loop through test files
-for i in "${!test_files[@]}"
+for i in "${!obj_files[@]}"
 do
     # Write current test case to log file
-    echo "Test case: ${test_files[$i]}" >> ${logfiles[$i]}
+    echo "Test case: ${obj_files[$i]}" >> ${logfiles[$i]}
     echo "--------------------------------" >> ${logfiles[$i]}
 
     # Loop through parameters
@@ -32,7 +26,7 @@ do
             echo "Number of threads: $numThread" >> ${logfiles[$i]}
 
             # Run test file with parameter
-            ./${test_files[$i]} $param $numThread >> ${logfiles[$i]}
+            ./${obj_files[$i]} $param $numThread >> ${logfiles[$i]}
             echo "--------------------------------" >> ${logfiles[$i]}
 
             # skip tests for multiple threads for the serial test
